cf4ce1f4ec4dc6b2b2faec1ca585f71927bd38eb
Will Lifferth <wlifferth@icloud.com>
2017-09-07 11:18:51 -0400

    Added functionality to allow multiple iterations

--- pair.py
+++ pair.py
@@ -15,32 +15,41 @@ def pair(mentor_csv, mentee_csv):
 
     mentee_pairing_options = {k: {} for k in mentees['UT NetID']}
     mentor_pairs = {k: [] for k in mentors['UT NetID']}
-    """
-    if len(mentees['UT NetID']) > 2 * len(mentors['UT NetID']):
+    
+    if len(mentees['UT NetID']) >= 2 * len(mentors['UT NetID']):
         print("Error! We have {} mentees but only {} mentors".format(len(mentees['UT NetID']), len(mentors['UT NetID'])))
-        return
-        """
 
     for i, mentee in mentees.iterrows():
         for j, mentor in mentors.iterrows():
             mentee_pairing_options[mentee['UT NetID']][mentor['UT NetID']] = score_pair(mentee, mentor)
 
-    print(mentee_pairing_options)
-
     # Now run our exhaustive search over the sample space
     # NAY! Simulated annealing to the rescue
-    available_mentors = list(mentors['UT NetID'])
-    available_mentors.extend(mentors['UT NetID'])
-    g = {k: '' for k in mentees['UT NetID']}
-    config_score = 0
-
-    for menteenid in g:
-        for mentornid, score in sorted(mentee_pairing_options[menteenid].items(), key=operator.itemgetter(1), reverse=True):
-            if mentornid in available_mentors:
-                g[menteenid] = mentornid
-                available_mentors.remove(mentornid)
-                config_score += score
-                break
+    available_mentors_template = list(mentors['UT NetID'])
+    available_mentors_template.extend(mentors['UT NetID'])
+    # Number of cycles to run through
+    # THIS IS THE NUMBER TO CHANGE TO RUN MORE ITERATIONS
+    #   |
+    #   V
+    n = 100
+    g = [{k: '' for k in mentees['UT NetID']} for x in range(n)]
+    config_score = [0 for x in range(n)]
+    carryover_term = 0
+    for run in range(n): 
+        # Implicit reshuffling of dictionary happens here
+        # Recreate a fresh availalbe_mentors list
+        available_mentors = list(available_mentors_template)
+        for menteenid in g[run]:
+            for mentornid, score in sorted(mentee_pairing_options[menteenid].items(), key=operator.itemgetter(1), reverse=True):
+                if mentornid in available_mentors:
+                    g[run][menteenid] = mentornid
+                    available_mentors.remove(mentornid)
+                    config_score[run] += score
+                    break
+        if config_score[run] > config_score[carryover_term]:
+            carryover_term = run
+    g = g[carryover_term]
+    config_score = config_score[carryover_term]
     for k in g:
         print("{}: {}".format(k, g[k]))
     print("Total Config Score: {}".format(config_score))
--- score_pair.py
+++ score_pair.py
@@ -1,19 +1,27 @@
 def score_pair(mentee, mentor):
-    score = 0
+    score = 0.
     if mentee['Major'] != mentor['Major']:
-        return 0
-    """
-    if mentee['Concentration'] == mentors['Concentration']:
-        score += 10
-        """
+        return score
+    if type(score) != type(1.):
+        print(type(score))
+        print(score)
     # Add together all quantitative answers
     for x in range(1, 10):
         a = str(x)
-        score += (5 - abs(mentee[a] - mentor[a])) ** 1.2
+        score += (5 - abs(mentee[a] - mentor[a])) ** 1.2 # Do not change or if you do, God help you
+        if type(score) != type(1.):
+            print(mentee[a])
+            print(mentor[a])
+    if type(score) != type(1.):
+        print(type(score))
+        print(score)
     # Figure out booleans
     mentee_interests = mentee['Interests'].split(', ')
     mentor_interests = mentor['Interests'].split(', ')
     avg_num_interests = (len(mentee_interests) + len(mentor_interests) + 1)/2
     score += sum([4 * avg_num_interests if x in mentor_interests else 0 for x in mentee_interests]) / avg_num_interests
-    return score
+    if type(score) != type(1.):
+        print(type(score))
+        print(score)
+    return int(score)
 
